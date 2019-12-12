8b528f95049bac402a12a9ac0448d1acc301f8c1
Satoshi Kojima <skoji@skoji.jp>
2019-12-12 18:30:06 +0900

    rename strip plugin

--- _plugins/strip_figure.rb
+++
@@ -1,8 +0,0 @@
-module Jekyll
-  module StripFigureFilter
-    def strip_figure(input)
-      input.sub(/<figure>.*?<\/figure>/m, '')
-    end      
-  end
-end
-Liquid::Template.register_filter(Jekyll::StripFigureFilter)
